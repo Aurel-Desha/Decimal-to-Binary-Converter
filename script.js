3ef3dd5ab9a8c905d1c9f97370ccbb41e940ba76
aurel-shael <deshyesguy2@gmail.com>
2024-03-04 01:06:07 +0100

    script

--- script.js
+++ script.js
@@ -2,9 +2,12 @@ const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
-const checkUserInput = () => {
-  if (!numberInput.value||) {
 
+
+const checkUserInput = () => {
+  if (!numberInput.value || isNaN(parseInt(numberInput.value))) {
+    alert("Please provide a decimal number");
+    return;
   }
 
   console.log(numberInput.value);
